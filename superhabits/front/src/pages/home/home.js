25b5cccb2cb5a17ef9beaefe9217259e57a3bf74
ShellOliver <shelldroid01@gmail.com>
2018-10-26 14:27:40 -0300

    added semantic-ui library with menu component working

--- superhabits/front/src/pages/home/home.js
+++ superhabits/front/src/pages/home/home.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { Menu, Segment } from 'semantic-ui-react'
 
-const Home = () => {
-  return (
-    <div>
-      aqui home
-    </div>
-  )
+export default class Home extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <div>
+        <Menu pointing secondary>
+          <Menu.Item
+            name='home'
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='doing'
+            active={activeItem === 'doing'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Menu position='right'>
+            <Menu.Item
+              name='logout'
+              active={activeItem === 'logout'}
+              onClick={this.handleItemClick}
+            />
+          </Menu.Menu>
+        </Menu>
+
+        <Segment>
+          created by <b>ShellOliver</b>
+        </Segment>
+      </div>
+    )
+  }
 }
-
-export default Home
-
