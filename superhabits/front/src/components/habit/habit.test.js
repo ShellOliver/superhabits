c77809cc75803cfa44faa677b45cf032d8799328
ShellOliver <shelldroid01@gmail.com>
2018-10-29 17:37:09 -0300

    added missing tests for habit redux structure

--- superhabits/front/src/components/habit/habit.test.js
+++ superhabits/front/src/components/habit/habit.test.js
@@ -1,6 +1,8 @@
-import { mount } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import React from 'react'
-import { Habit } from './habit.component'
+import HabitCon, { Habit } from './habit.component'
+import configureStore from 'redux-mock-store'
+import habitReducer from '../../reducers/habit.reducer'
 
 describe('habit page test', () => {
   let wrapper
@@ -21,5 +23,27 @@ describe('habit page test', () => {
     input.simulate('change', target.value = 'dasfaga')
     input.simulate('submit')
     expect(wrapper.instance().onsubmit).toBeCalled()
+  })
+})
+
+describe('habit action test', () => {
+  let wrapperCon
+  const store = configureStore()({
+    habit: [{ name: 'item1' }, { name: 'item2' }]
+  })
+
+  beforeEach(() => {
+    wrapperCon = shallow(<HabitCon store={store} />)
+  })
+  it('should return addHabit action when call addHabit from component', () => {
+    const addHab = wrapperCon.props().addHabit({ name: 'added by test' })
+    expect(addHab).toEqual({ type: 'ADD_HABIT', payload: { name: 'added by test' } })
+  })
+})
+
+describe('habit reducer test', () => {
+  it('should add habit to store', () => {
+    let newState = habitReducer(undefined, { type: 'ADD_HABIT', payload: { name: 'added by test' } })
+    expect(newState).toEqual({ habits: [ { name: 'added by test' } ] })
   })
 })
