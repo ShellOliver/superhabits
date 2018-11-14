c5b1525dd4a81edaaade0054f67cc61efd3a7f35
ShellOliver <shelldroid01@gmail.com>
2018-11-13 23:11:46 -0300

    added redux thunk middleware for simplify the use of actions and states

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import reducers from '../reducers'
+import ReduxThunk from 'redux-thunk'
 
-export default createStore(reducers)
+export default createStore(reducers, applyMiddleware(ReduxThunk))
