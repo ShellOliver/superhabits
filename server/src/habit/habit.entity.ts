13557fbf712279a31a02267503c288a6ed18aecc
ShellOliver <shelldroid01@gmail.com>
2018-12-03 22:48:43 -0300

    add name field to habits and cors for backend

--- server/src/habit/habit.entity.ts
+++ server/src/habit/habit.entity.ts
@@ -1,7 +1,10 @@
-import {Entity, PrimaryGeneratedColumn} from 'typeorm';
+import {Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
 
 @Entity()
 export class Habit {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({nullable: true})
+  name: string;
 }
